a4c9c8a5af1fd2c4671dcf9f147918ae143c1f2f
nicmanone <nicmanone@mail.ru>
2026-09-21 05:49:15 +0300

    add db

--- app.rb
+++ app.rb
@@ -2,11 +2,38 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
+
+configure do
+  init_db
+  @db.execute <<~SQL
+    CREATE TABLE IF NOT EXISTS Posts (
+	  id	INTEGER PRIMARY KEY AUTOINCREMENT,	
+      created_date	TEXT,
+	  content	TEXT
+    )
+  SQL
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/new' do
-  erb  "Hello World"
+  erb  :new
+end
+
+post '/new' do
+  content = params[:content]
+  erb "You typed #{content}"
 end
